a52a2468592580108d86ee3057c2f6af02400192
Gabriel Lewis <gabrielhlewis@gmail.com>
2017-04-10 14:37:38 -0700

    Linting

--- server.js
+++ server.js
@@ -1,33 +1,34 @@
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const Mailchimp = require('mailchimp-api-v3')
+const express = require('express');
 
-app.use(bodyParser.json())
-app.set('port', (process.env.PORT || 3001))
+const app = express();
+const bodyParser = require('body-parser');
+const Mailchimp = require('mailchimp-api-v3');
 
-const isProd = process.env.NODE_ENV === 'production'
-console.log('Starting prod =', isProd)
+app.use(bodyParser.json());
+app.set('port', (process.env.PORT || 3001));
+
+const isProd = process.env.NODE_ENV === 'production';
+console.log('Starting prod =', isProd);
 if (isProd) {
-  app.use(express.static('client/build'))
+  app.use(express.static('client/build'));
 }
 
 const mailApi = new Mailchimp('83ffb81bbb5be6a9198f77892e614f85-us14');
 
 app.post('/api/join', (req, res) => {
-  const { email = '', name = '', company = '' } = req.body
+  const { email = '', name = '', company = '' } = req.body;
   console.log(email, name, company);
-  if (email.split('@').length !== 2 ) {
-    res.status(400).send({ status: 400 })
-    return
+  if (email.split('@').length !== 2) {
+    res.status(400).send({ status: 400 });
+    return;
   }
 
-  let firstName = name
-  let lastName = ''
-  const split = name.trim().indexOf(' ')
+  let firstName = name;
+  let lastName = '';
+  const split = name.trim().indexOf(' ');
   if (split > 0) {
-    firstName = name.substring(0, split)
-    lastName = name.substring(split)
+    firstName = name.substring(0, split);
+    lastName = name.substring(split);
   }
 
   const body = {
@@ -38,19 +39,19 @@ app.post('/api/join', (req, res) => {
       LNAME: lastName,
       CNAME: company,
     },
-  }
+  };
   if (isProd) {
     mailApi.post('lists/3f0a35ceac/members', body).then(() => {
-      console.log('Success')
+      console.log('Success');
     }).catch((e) => {
-      console.error('Failed')
-    })
+      console.error('Failed');
+    });
   }
 
-  const resp = {}
-  res.json(resp)
+  const resp = {};
+  res.json(resp);
 });
 
 app.listen(app.get('port'), () => {
-  console.log(`Server started at http://localhost:${app.get('port')}/`) // eslint-disable-line no-console
-})
+  console.log(`Server started at http://localhost:${app.get('port')}/`); // eslint-disable-line no-console
+});
